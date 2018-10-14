29b0897ee4ab7024af758c49a06259dc92a50a3b
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2018-10-14 13:30:56 +0200

    Add headings

--- z01/z01.cpp
+++ z01/z01.cpp
@@ -165,9 +165,16 @@ void zadanie4() {
 
 int main() {
 
+    cout << "Zadanie 1. \n";
     zadanie1();
+
+    cout << "\n\n Zadanie 2.\n";
     zadanie2();
+
+    cout << "\n\n Zadanie 3.\n";
     zadanie3();
+
+    cout << "\n\n Zadanie 4.\n";
     zadanie4();
 
     return 0;
