ff6c8beca5116ef79d9f88193a84e7546d31498c
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2018-10-14 12:17:28 +0200

    Add exercise 1

--- z01/z01.cpp
+++ z01/z01.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void zadanie1() {
+    double wzrost;
+    double waga;
+    double bmi;
+
+    cout << "Podaj swoj wzrost (w metrach): ";
+    cin >> wzrost;
+    
+    cout << "\nPodaj swoja wage (w kilogramach): ";
+    cin >> waga;
+
+    bmi = waga / (wzrost * wzrost);
+    cout << "\nTwoje bmi to: " << bmi << "\n";
+}
+
 int main() {
 
-    cout << "Hello dupa" << endl;
-
+    zadanie1();
+    
     return 0;
 }
